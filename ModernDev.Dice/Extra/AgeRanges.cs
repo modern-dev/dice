bffca2d9319a1d47c94260cbffc1bc055c793659
Bohdan Shtepan <winsatana@ya.ru>
2016-05-11 03:25:30 +0300

    Adds AgeRanges enum.

---
+++ ModernDev.Dice/Extra/AgeRanges.cs
@@ -0,0 +1,45 @@
+﻿//
+// This file\code is part of InTouch project.
+//
+// InTouch - is a .NET wrapper for the vk.com API.
+// https://github.com/virtyaluk/InTouch
+//
+// Copyright (c) 2016 Bohdan Shtepan
+// http://modern-dev.com/
+//
+// Licensed under the MIT license.
+//
+
+namespace ModernDev.Dice
+{
+    /// <summary>
+    /// Age ranges.
+    /// </summary>
+    public enum AgeRanges
+    {
+        /// <summary>
+        /// Child range.
+        /// </summary>
+        Child,
+
+        /// <summary>
+        /// Teen range.
+        /// </summary>
+        Teen,
+
+        /// <summary>
+        /// Adult range.
+        /// </summary>
+        Adult,
+
+        /// <summary>
+        /// Senior range.
+        /// </summary>
+        Senior,
+
+        /// <summary>
+        /// All range.
+        /// </summary>
+        All
+    }
+}
