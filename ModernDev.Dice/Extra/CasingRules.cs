8737b950bcb0237818c0f656fb1dc907cfba5248
Bohdan Shtepan <winsatana@ya.ru>
2016-05-11 03:25:40 +0300

    Adds CasingRules enum.

---
+++ ModernDev.Dice/Extra/CasingRules.cs
@@ -0,0 +1,35 @@
+﻿//
+// This file\code is part of InTouch project.
+//
+// InTouch - is a .NET wrapper for the vk.com API.
+// https://github.com/virtyaluk/InTouch
+//
+// Copyright (c) 2016 Bohdan Shtepan
+// http://modern-dev.com/
+//
+// Licensed under the MIT license.
+//
+
+namespace ModernDev.Dice
+{
+    /// <summary>
+    /// Casing rules.
+    /// </summary>
+    public enum CasingRules
+    {
+        /// <summary>
+        /// Lower case.
+        /// </summary>
+        LowerCase,
+
+        /// <summary>
+        /// Upper case.
+        /// </summary>
+        UpperCase,
+
+        /// <summary>
+        /// Mixed case.
+        /// </summary>
+        MixedCase
+    }
+}
